2d80f5b4292cf5a0871c1a8d1676b39c1398b048
Kyle McDonald <kyle@kylemcdonald.net>
2013-06-06 09:41:00 -0500

    hosting mouseCollection on status.json

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/status.json', function(req, res){
+  res.send(mouseCollection);
+});
+
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
